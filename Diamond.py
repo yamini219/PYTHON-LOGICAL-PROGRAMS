405f104e66e03bc681b788a300a68ea7ec7a4d15
yamini219 <64453110+yamini219@users.noreply.github.com>
2020-12-12 18:40:01 +0530

    CODE AND OUTPUT

---
+++ Diamond.py
@@ -0,0 +1,16 @@
+num=int(input("number:"))
+space=num-1
+for i in range(num):
+  for j in range(space-i):
+    print(end=" ")
+  for k in range(i+1):
+    print("*",end=" ")
+  print("\r")
+num=num-1
+for i in range(num):
+  for j in range(i+1):
+    print(end=" ")
+  for k in range(num-i):
+    print("*",end=" ")
+  print("\r")
+    
