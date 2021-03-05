73e0f58a0d138ecb21c741c94a3a125d64088072
yamini219 <64453110+yamini219@users.noreply.github.com>
2021-03-05 20:59:14 +0530

    CODE AND OUTPUT

---
+++ Linear search.py
@@ -0,0 +1,13 @@
+list=[]
+n=int(input("Enter the length of the list:"))
+for i in range(n):
+  x=int(input("Enter the value:"))
+  list.append(x)
+list.sort()
+print(list)
+Target=int(input("Enter the target:"))
+if Target==x:
+  print("Target is found")
+else:
+  print("Target is not found")
+
